75c206d7ba7d1dab27b579406ac39aee90afb1a8
Gabriel Vidal <64874245+gabrielvm98@users.noreply.github.com>
2023-08-20 00:25:47 -0500

    Add files via upload

---
+++ CalculadoraCalificacionesApp1/CalculadoraCalificacionesApp1/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿
+namespace CalculadoraCalificacionesApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.labelPromedio = new System.Windows.Forms.Label();
+            this.buttonLimpiar = new System.Windows.Forms.Button();
+            this.buttonCalcular = new System.Windows.Forms.Button();
+            this.buttonReporte = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(21, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(260, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calculadora de Calificaciones";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(12, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Matemática";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(12, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 21);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Lengua";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(12, 189);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 21);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Ciencia";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(12, 149);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 21);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Ingles";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(12, 229);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(64, 21);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Historia";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(172, 71);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(109, 27);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox2.Location = new System.Drawing.Point(172, 111);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(109, 27);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox3.Location = new System.Drawing.Point(172, 191);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(109, 27);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox4.Location = new System.Drawing.Point(172, 151);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(109, 27);
+            this.textBox4.TabIndex = 8;
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox5.Location = new System.Drawing.Point(172, 231);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(109, 27);
+            this.textBox5.TabIndex = 10;
+            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(12, 283);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(85, 21);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Promedio :";
+            // 
+            // labelPromedio
+            // 
+            this.labelPromedio.AutoSize = true;
+            this.labelPromedio.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelPromedio.Location = new System.Drawing.Point(232, 283);
+            this.labelPromedio.Name = "labelPromedio";
+            this.labelPromedio.Size = new System.Drawing.Size(0, 21);
+            this.labelPromedio.TabIndex = 12;
+            // 
+            // buttonLimpiar
+            // 
+            this.buttonLimpiar.Location = new System.Drawing.Point(12, 350);
+            this.buttonLimpiar.Name = "buttonLimpiar";
+            this.buttonLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.buttonLimpiar.TabIndex = 13;
+            this.buttonLimpiar.Text = "Limpiar";
+            this.buttonLimpiar.UseVisualStyleBackColor = true;
+            this.buttonLimpiar.Click += new System.EventHandler(this.buttonLimpiar_Click);
+            // 
+            // buttonCalcular
+            // 
+            this.buttonCalcular.Location = new System.Drawing.Point(206, 350);
+            this.buttonCalcular.Name = "buttonCalcular";
+            this.buttonCalcular.Size = new System.Drawing.Size(75, 23);
+            this.buttonCalcular.TabIndex = 14;
+            this.buttonCalcular.Text = "Calcular ";
+            this.buttonCalcular.UseVisualStyleBackColor = true;
+            this.buttonCalcular.Click += new System.EventHandler(this.buttonCalcular_Click);
+            // 
+            // buttonReporte
+            // 
+            this.buttonReporte.Location = new System.Drawing.Point(206, 392);
+            this.buttonReporte.Name = "buttonReporte";
+            this.buttonReporte.Size = new System.Drawing.Size(75, 23);
+            this.buttonReporte.TabIndex = 15;
+            this.buttonReporte.Text = "Reporte";
+            this.buttonReporte.UseVisualStyleBackColor = true;
+            this.buttonReporte.Click += new System.EventHandler(this.buttonReporte_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(301, 427);
+            this.Controls.Add(this.buttonReporte);
+            this.Controls.Add(this.buttonCalcular);
+            this.Controls.Add(this.buttonLimpiar);
+            this.Controls.Add(this.labelPromedio);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Calculadora";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label labelPromedio;
+        private System.Windows.Forms.Button buttonLimpiar;
+        private System.Windows.Forms.Button buttonCalcular;
+        private System.Windows.Forms.Button buttonReporte;
+    }
+}
+
---
+++ CalculadoraCalificacionesApp1/CalculadoraCalificacionesApp1/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculadoraCalificacionesApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Asignacion de Validacion al escribir en los cuadros de texto
+            textBox1.TextChanged += TextBox_TextChanged;
+            textBox2.TextChanged += TextBox_TextChanged;
+            textBox3.TextChanged += TextBox_TextChanged;
+            textBox4.TextChanged += TextBox_TextChanged;
+            textBox5.TextChanged += TextBox_TextChanged;
+        }
+
+        // Evento del boton para limpiar los campos
+        private void buttonLimpiar_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+            labelPromedio.Text = "";
+        }
+
+        // Evento del boton para calcular el promedio
+        private void buttonCalcular_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Intenta convertir los valores de los cuadros de texto a decimales
+                double calificacion1 = Convert.ToDouble(textBox1.Text);
+                double calificacion2 = Convert.ToDouble(textBox2.Text);
+                double calificacion3 = Convert.ToDouble(textBox3.Text);
+                double calificacion4 = Convert.ToDouble(textBox4.Text);
+                double calificacion5 = Convert.ToDouble(textBox5.Text);
+
+                // Validar si las calificaciones son válidas
+                if (!IsValidCalificacion(calificacion1) || !IsValidCalificacion(calificacion2) ||
+                    !IsValidCalificacion(calificacion3) || !IsValidCalificacion(calificacion4) ||
+                    !IsValidCalificacion(calificacion5))
+                {
+                    throw new ArgumentOutOfRangeException("Las calificaciones deben estar entre 0 y 100.");
+                }
+
+                // Crea una instancia de CalculadoraPromedio y agrega calificaciones
+                CalculadoraPromedio calculadoraPromedio = new CalculadoraPromedio();
+                calculadoraPromedio.AddCalificacion(calificacion1);
+                calculadoraPromedio.AddCalificacion(calificacion2);
+                calculadoraPromedio.AddCalificacion(calificacion3);
+                calculadoraPromedio.AddCalificacion(calificacion4);
+                calculadoraPromedio.AddCalificacion(calificacion5);
+
+                // Calcula el promedio y lo muestra en el Label de promedio
+                double promedio = calculadoraPromedio.CalcularPromedio();
+                labelPromedio.Text = promedio.ToString("F2");
+
+
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Ingrese calificaciones válidas en todos los campos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Evento del boton para generar el reporte
+        private void buttonReporte_Click(object sender, EventArgs e)
+        {
+
+            // Verifica si el Label de promedio está vacío
+            if (string.IsNullOrWhiteSpace(labelPromedio.Text))
+            {
+                MessageBox.Show("No se puede generar el reporte sin un promedio calculado.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Genera el nombre del archivo con fecha y hora actual
+            string nombreArchivo = $"reporte-{DateTime.Now:yyyyMMddHHmmss}.csv";
+            string rutaArchivo = @"C:\Users\Usuario\source\repos\CalculadoraCalificacionesApp1\CalculadoraCalificacionesApp1\";
+
+            // Genera el reporte en un archivo CSV
+            using (StreamWriter writer = new StreamWriter(rutaArchivo + nombreArchivo))
+            {
+                writer.WriteLine("Calificaciones");
+                writer.WriteLine($"{label2.Text},{textBox1.Text}");
+                writer.WriteLine($"{label3.Text},{textBox2.Text}");
+                writer.WriteLine($"{label4.Text},{textBox3.Text}");
+                writer.WriteLine($"{label5.Text},{textBox4.Text}");
+                writer.WriteLine($"{label6.Text},{textBox5.Text}");
+                writer.WriteLine($"Promedio: {labelPromedio.Text}");
+            }
+            MessageBox.Show("Reporte generado correctamente.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        // Funcion para Validacion del ingreso de calificaciones
+        private void TextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (sender is TextBox textBox)
+            {
+                if (!string.IsNullOrWhiteSpace(textBox.Text) && !IsValidDecimal(textBox.Text))
+                {
+                    MessageBox.Show("Ingrese un valor decimal válido entre 0 y 100.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    textBox.Text = "";
+                }
+            }
+        }
+
+        // Funciones auxiliares para validar que lo ingresado sea un numero decimal válido entre 0 y 100
+        private bool IsValidDecimal(string input)
+        {
+            if (double.TryParse(input, out double value))
+            {
+                return IsValidCalificacion(value);
+            }
+            return false;
+        }
+
+        private bool IsValidCalificacion(double calificacion)
+        {
+            return calificacion >= 0 && calificacion <= 100;
+        }
+    }
+
+
+    // Clase implementada para el calculo del promedio
+    class CalculadoraPromedio
+    {
+        private List<double> calificaciones { get; set; }
+
+        public CalculadoraPromedio()
+        {
+            this.calificaciones = new List<double>();
+        }
+
+        public void AddCalificacion(double calificacion)
+        {
+            calificaciones.Add(calificacion);
+        }
+
+        public double CalcularPromedio()
+        {
+            double suma = 0;
+            foreach (double calificacion in calificaciones)
+            {
+                suma += calificacion;
+            }
+            return calificaciones.Count > 0 ? suma / calificaciones.Count : 0;
+        }
+    }
+}
